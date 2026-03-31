99cf84c38ae8582da368e2ea80e7a39e416bff8b
Ziroch_Capalay <v.holubenko09@gmail.com>
2026-03-31 21:58:52 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let total = days * 40;
+  const LONG_TERM = 7;
+  const MEDIUM_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const MEDIUM_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    total -= LONG_TERM_DISCOUNT;
+  } else if (days >= MEDIUM_TERM) {
+    total -= MEDIUM_TERM_DISCOUNT;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
